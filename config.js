813cf0fd89ee604a14a0d541f7f5cb04f42d5c25
qtrueky <firechicken1210b.dc@gmail.com>
2026-07-15 22:08:39 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -2,23 +2,75 @@
 // 這裡填寫你的 LIFF ID
 const MY_LIFF_ID = "2010713742-gDKkGt5y"; 
 
-// 這裡管理你的圖片清單
-// 未來新增圖片，只要把檔案放進 images 資料夾，並在這裡新增一行即可
+// 這裡管理你的圖片清單 (由 update-config.js 自動產生)
 const memeList = [
-    { fileName: "01.png" },
-    { fileName: "02.png" },
-    { fileName: "03.png" },
-    { fileName: "04.png" },
-    { fileName: "05.png" },
-    { fileName: "06.png" },
-    { fileName: "07.png" },
-    { fileName: "08.png" },
-    { fileName: "09.png" },
-    { fileName: "10.png" },
-    { fileName: "11.png" },
-    { fileName: "12.png" },
-    { fileName: "13.png" },
-    { fileName: "14.png" },
-    { fileName: "15.png" },
-    { fileName: "16.png" },
+    {
+        "fileName": "01.png"
+    },
+    {
+        "fileName": "02.png"
+    },
+    {
+        "fileName": "03.png"
+    },
+    {
+        "fileName": "04_.png"
+    },
+    {
+        "fileName": "04.png"
+    },
+    {
+        "fileName": "05.png"
+    },
+    {
+        "fileName": "06.png"
+    },
+    {
+        "fileName": "07.png"
+    },
+    {
+        "fileName": "08.png"
+    },
+    {
+        "fileName": "09 (1).png"
+    },
+    {
+        "fileName": "09.png"
+    },
+    {
+        "fileName": "10.png"
+    },
+    {
+        "fileName": "11_.png"
+    },
+    {
+        "fileName": "11.png"
+    },
+    {
+        "fileName": "12.png"
+    },
+    {
+        "fileName": "13_.png"
+    },
+    {
+        "fileName": "13.png"
+    },
+    {
+        "fileName": "14_.png"
+    },
+    {
+        "fileName": "14.png"
+    },
+    {
+        "fileName": "15_.png"
+    },
+    {
+        "fileName": "15.png"
+    },
+    {
+        "fileName": "16.png"
+    },
+    {
+        "fileName": "17.png"
+    }
 ];
