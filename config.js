7d6c2b4e4d8a80310ff26eea49a849ad2266184e
qtrueky <firechicken1210b.dc@gmail.com>
2026-07-15 20:33:05 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,24 @@
+// ====== config.js ======
+// 這裡填寫你的 LIFF ID
+const MY_LIFF_ID = "2010713742-gDKkGt5y"; 
+
+// 這裡管理你的圖片清單
+// 未來新增圖片，只要把檔案放進 images 資料夾，並在這裡新增一行即可
+const memeList = [
+    { fileName: "01.png" },
+    { fileName: "02.png" },
+    { fileName: "03.png" },
+    { fileName: "04.png" },
+    { fileName: "05.png" },
+    { fileName: "06.png" },
+    { fileName: "07.png" },
+    { fileName: "08.png" },
+    { fileName: "09.png" },
+    { fileName: "10.png" },
+    { fileName: "11.png" },
+    { fileName: "12.png" },
+    { fileName: "13.png" },
+    { fileName: "14.png" },
+    { fileName: "15.png" },
+    { fileName: "16.png" },
+];
